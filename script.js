99b7f8d2e5f154d44803ce5dd11e6b23100833e8
ProfThunderberry <67694168+ProfThunderberry@users.noreply.github.com>
2022-10-11 15:50:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function loadNavbar() {
-	console.log(this.location.pathname);
-	$.get("features/navbar.html", function(data){
+	
+	const filePath = this.location.pathname;
+    const fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
+
+    let navbarFilePath = "features/navbar.html";
+    if (!(fileName === "index.html" || fileName === "")) navbarFilePath = "../" + navbarFilePath;
+	
+	$.get(navbarFilePath, function(data){
+		console.log(data);
 		$("nav.navbar-placeholder").replaceWith(data);
 	});
 }
