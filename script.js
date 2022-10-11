4fed7e978ea6c28ef16f75a52f48d4b4bab58e0b
ProfThunderberry <67694168+ProfThunderberry@users.noreply.github.com>
2022-10-11 15:40:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function loadNavbar() {
 	console.log(this.location.pathname);
 	$.get("features/navbar.html", function(data){
-		$("#navbar-placeholder").replaceWith(data);
+		$(".navbar-placeholder").replaceWith(data);
 	});
 }
